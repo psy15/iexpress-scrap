77db88add60f03137dca3dd52e4dc14092619c36
Paras <parasy152@gmail.com>
2022-01-02 12:40:22 +0530

    some code improvements

--- app.py
+++ app.py
@@ -35,25 +35,25 @@ gh = github.Github(GTOKEN)
 gist = gh.get_gist(GIST_ID)
 
 
-def read_last_posted_url():
+def read_last_posted_url_from_gist() -> str:
     try:
         file = gist.files['ie_url'].content
         return file.strip()
     except:
-        log.exception("Error reading url file")
+        log.exception("Error reading gist")
 
 
-def write_last_posted_url(url):
+def write_last_posted_url_to_gist(url: str) -> None:
     try:
         gist.edit(
             description="using for heroku",
             files={"ie_url": github.InputFileContent(content=url[0])},
         )
     except:
-        log.exception("Error writing url file")
+        log.exception("Error writing gist")
 
 
-def get_list_of_urls():
+def get_list_of_urls() -> dict:
 
     response = requests.get(TARGET_URL)
     soup = BeautifulSoup(response.content, 'html.parser')
@@ -74,8 +74,7 @@ def get_list_of_urls():
             title.append(i.text)
             all_urls.append(i.get('href'))
 
-    last_url = str(read_last_posted_url())
-
+    last_url = read_last_posted_url_from_gist()
     urls_to_post = {}
 
     for i, j in enumerate(all_urls):
@@ -96,22 +95,22 @@ def get_list_of_urls():
             urls_to_post[i].append('')
             continue
 
-        #make tag lowercase and prepend with #
+        #make tag lowercase, replace spaces with '_' and prepend str    23 with #
         for j in data.find_all('a'):
-            tags += ' #' + j.text.lower().replace(' ', '_').replace('-', '_')
+            tags += f" #{j.text.lower().replace(' ', '_').replace('-', '_')}"
 
-        urls_to_post[i].append(tags[1:])
+        urls_to_post[i].append(tags.lstrip())
 
     # cwrite to file heck if dictionary if empty
     if bool(urls_to_post):
-        write_last_posted_url(list(urls_to_post)[:1])
+        write_last_posted_url_to_gist(list(urls_to_post)[:1])
         return urls_to_post
 
     log.info('no new posts found..')
     exit()  # exit if empty
 
 
-def post():
+def post() -> None:
     bot = telegram.Bot(token=TOKEN)
     dict_ = get_list_of_urls()
 
@@ -119,15 +118,23 @@ def post():
 
     for i in reversed(dict_):
 
-        iv = f"http://t.me/iv?url={i}&rhash=1398b799d706ac"
+        iv_url = f"http://t.me/iv?url={i}&rhash=1398b799d706ac"
 
         # dict[i][0] is url, dict[i][1] is tag string
         message = message_template.format(
-            link=iv, title=dict_[i][0], text="🔗", tags=dict_[i][1])
-        log.info(f"posting message: {message}")
-        bot.sendMessage(chat_id=CHANNEL,
-                        parse_mode=telegram.ParseMode.HTML, text=message,
-                        disable_web_page_preview=False)
+            link=iv_url,
+            title=dict_[i][0],
+            text="🔗",
+            tags=dict_[i][1]
+        )
+        log.info("posting message: %s", message)
+
+        bot.sendMessage(
+            chat_id=CHANNEL,
+            parse_mode=telegram.ParseMode.HTML,
+            text=message,
+            disable_web_page_preview=False
+        )
 
         # if last message then break, break without delay
         if i == list(dict_)[0]:
@@ -137,7 +144,6 @@ def post():
 
 
 def main() -> None:
-
     post()
 
 
